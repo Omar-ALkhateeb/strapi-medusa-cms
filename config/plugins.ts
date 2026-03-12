918ad5a2d103753cffbf59287a9e9b022df7e92a
Omar-Alkhateeb <omar.alkhateeb9919@gmail.com>
2026-03-12 16:32:05 +0300

    first commit

--- config/plugins.ts
+++ config/plugins.ts
@@ -1,5 +1,25 @@
-import type { Core } from '@strapi/strapi';
-
-const config = ({ env }: Core.Config.Shared.ConfigParams): Core.Config.Plugin => ({});
-
-export default config;
+export default ({ env }) => ({
+  upload: {
+    config: {
+      provider: "aws-s3",
+      providerOptions: {
+        s3Options: {
+          credentials: {
+            accessKeyId: env("DO_SPACE_ACCESS_KEY"),
+            secretAccessKey: env("DO_SPACE_SECRET_KEY"),
+          },
+          endpoint: env("DO_SPACE_ENDPOINT"), // e.g., 'https://nyc3.digitaloceanspaces.com'
+          region: env("DO_SPACE_REGION"), // e.g., 'nyc3'
+          params: {
+            Bucket: env("DO_SPACE_BUCKET"),
+          },
+        },
+      },
+      actionOptions: {
+        upload: {},
+        uploadStream: {},
+        delete: {},
+      },
+    },
+  },
+});
